8c4a355e52595095acd2676d09052483ccc3f759
mohammad saleh <salehos763@gmail.com>
2018-11-23 00:29:43 +0330

    Finish That Fucking 4

--- main/form.py
+++ main/form.py
@@ -1,12 +1,23 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            raise ValidationError("There is no user registered with the specified email address!")
+        return email
 
     class Meta:
         model = User
@@ -20,4 +31,3 @@ class ContactForm(forms.Form):
         widget=forms.Textarea
     )
     email = forms.EmailField(required=True)
-
--- main/views.py
+++ main/views.py
@@ -1,12 +1,12 @@
-from email.message import EmailMessage
-
-import self as self
 from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.models import User
+from django.core.checks import messages
+from django.core.exceptions import ValidationError
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-
 # Create your views here.
-from django.template.loader import get_template
+
 
 from main.form import SignUpForm, ContactForm
 
@@ -25,16 +25,6 @@ def login_(request):
     return render(request, 'login.html')
 
 
-#
-# def contactus(request):
-#     if request.method == 'POST':
-#         text = request.POST.get('text')
-#         title = request.POST.get('title')
-#         emamil = request.POST.get('email')
-#
-#     return render(request, 'contactus.html')
-
-
 def contactus(request):
     form_class = ContactForm
 
@@ -42,30 +32,33 @@ def contactus(request):
     if request.method == 'POST':
         form = form_class(data=request.POST)
         if form.is_valid:
-            # title = request.POST.get(
-            #     'title'
-            #     , '')
-            # email = request.POST.get(
-            #     'email'
-            #     , '')
-            # text = request.POST.get('text', '')
-            #
-            # # Email the profile with the
-            # # contact information
+            title = request.POST.get(
+                'title'
+                , '')
+            myemail = request.POST.get(
+                'email'
+                , '')
+            text = request.POST.get('text', '')
+
+            # Email the profile with the
+            # contact information
             # template = get_template('contact_template.txt')
             # context = {
-            #     'text': text,
+            #     'title': title,
             #     'email': email,
             #     'text': text,
             # }
             # content = template.render(context)
-            #
+
             # email = EmailMessage(
-            #     "New contact form submission",
-            #     content,
-            #     "Your website" + '',
+            #     title,
+            #     text,
+            #     myemail,
             #     ['ostadju@fastmail.com'],
-            #     headers={'Reply-To': email}
+            #     '',
+            #     reply_to=[],
+            #     headers={'Message-ID': 'foo'},
+
             # )
             # email.send()
             return render(request, 'itsok.html', )
@@ -80,11 +73,26 @@ def logout_(request):
 
 
 def signup(request):
+    context = {
+        "error1":False,
+        "error2":False,
+        "error3": False
+    }
+
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+        if User.objects.filter(email=request.POST.get('email')).exists():
+            context["error3"] = True
+        if request.POST.get('password1') != request.POST.get('password2'):
+            context["error2"] = True
+        if User.objects.filter(username=request.POST.get('username')).exists():
+            context["error1"] = True
+        if context["error1"] or context["error2"] or context["error3"]:
+            return render(request, "signup.html", context)
         if form.is_valid():
             form.save()
             return redirect('')
+
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
@@ -103,13 +111,13 @@ def editnameandusername(request):
     if request.method == 'POST':
         firstname = request.POST.get('firstname')
         lastname = request.POST.get('lastname')
-        gender =  request.POST.get('gender')
+        gender = request.POST.get('gender')
         bio = request.POST.get('bio')
         request.user.first_name = firstname
         request.user.last_name = lastname
 
         request.user.save();
         return render(request, 'profile.html', {'username': request.user.username, 'firstname': request.user.first_name,
-                                            'lastname': request.user.last_name}  )
-    return render(request, 'editnameandusername.html' , )
-
+                                                'lastname': request.user.last_name, 'bio': request.user.bio,
+                                                'gender': request.user.gender})
+    return render(request, 'editnameandusername.html', )
